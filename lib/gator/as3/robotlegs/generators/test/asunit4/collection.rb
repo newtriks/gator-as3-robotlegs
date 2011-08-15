9a2bef044e36432fd4bab8285aae7d3d9b5570cc
Simon Bailey <simon@newtriks.com>
2011-08-16 00:27:00 +0100

    Renaming and started work on AllTests.as
    Signed-off-by: Simon Bailey <simon@newtriks.com>

--- lib/gator/as3/robotlegs/generators/test/asunit4/collection.rb
+++ lib/gator/as3/robotlegs/generators/test/asunit4/collection.rb
@@ -1,6 +1,7 @@
 require File.dirname(__FILE__) + '/command'
 require File.dirname(__FILE__) + '/mediator'
 require File.dirname(__FILE__) + '/view'
+require File.dirname(__FILE__) + '/suite'
 
 module Gator
   module AS3
@@ -15,6 +16,7 @@ module Gator
           register_subcommand CommandTestGenerator
           register_subcommand MediatorTestGenerator
           register_subcommand ViewTestGenerator
+          register_subcommand SuiteGenerator
           
         end
       end
--- lib/gator/as3/robotlegs/generators/test/asunit4/command.rb
+++ lib/gator/as3/robotlegs/generators/test/asunit4/command.rb
@@ -26,6 +26,7 @@ module Gator
           end
 
           def generate_implementation
+            invoke resolve_subcommand(["test","suite"],["as3","klass"]), [classname]
             return unless options[:impl]
             invoke resolve_subcommand(["robotlegs","command"],["as3","klass"])
           end
